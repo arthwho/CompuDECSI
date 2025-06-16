291e04572279fa7a625a09a8940ffabaf4e21f3c
Arthur Coelho <arthur.sfc@aluno.ufop.edu.br>
2025-06-15 22:55:23 -0300

    App base being built

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:compudecsi/view/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        textTheme: GoogleFonts.latoTextTheme(Theme.of(context).textTheme)
+            .copyWith(
+              bodyMedium: GoogleFonts.poppins(
+                textStyle: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ),
+      ),
+      home: const OnboardingScreen(),
+    );
+  }
+}
---
+++ lib/view/onboarding_screen.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 50),
+              Text(
+                'Bem vindo ao CompuDecsi!',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 27,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+
+              SizedBox(height: 5),
+
+              Text(
+                "Acompanhe os eventos da Semana da Computação no DESCI!",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+
+              SizedBox(height: 50),
+
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Image.asset('assets/undraw_amusement-park_j8fe.png'),
+              ),
+
+              SizedBox(height: 50),
+
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  height: 300,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 10,
+                        offset: Offset(0, -10),
+                        spreadRadius: 1,
+                      ),
+                    ],
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(16),
+                      topRight: Radius.circular(16),
+                    ),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      SizedBox(height: 20),
+
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: Text(
+                          'A plataforma de eventos da Semana da Computação no DESCI',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(height: 20),
+
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: Text(
+                          'O aplicativo onde você pode acompanhar a programação de eventos da Semana da Computação no DESCI, fazer check-in e perguntas para os palestrantes em tempo real!',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(height: 20),
+
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: MaterialButton(
+                          height: 50,
+                          minWidth: double.infinity,
+                          color: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          elevation: 2,
+                          onPressed: () {},
+                          child: Text(
+                            'Participar agora',
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 102, 29, 29),
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
